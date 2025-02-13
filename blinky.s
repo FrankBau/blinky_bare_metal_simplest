1b321f2dd17223f7e661027ec8cea6f102f827f5
Frank Bauernoeppel <frank@bauernoeppel.de>
2025-02-13 12:32:37 +0100

    anothe cleanup round

--- blinky.s
+++ blinky.s
@@ -22,22 +22,26 @@ isr_vectors:
 .global   _start
 .type     _start, %function
 _start:
-    ldr     r0, =0x40021000     // load MMIO address of RCC peripheral block to register r0
+    ldr     r0, =0x40021000     // load address of RCC peripheral block to r0
     ldr     r1, [r0, #0x4C]     // load content of RCC AHB2ENR register to r1
     movs    r2, #2              // load bit mask for GPIO port B to register r2
     orrs    r1, r2              // set bit to enable clock for GPIOB
-    str     r1, [r0, #0x4C]     // store (indirect) modified value to RCC AHB2ENR register
+    str     r1, [r0, #0x4C]     // store modified value to RCC AHB2ENR register
 
-    ldr     r0, =0x48000400     // load address of GPIOB peripheral block to register r0
-    movs    r1, 0x40            // load bitmask 1<<(2*3) to register r1
-    str     r1, [r0, #0x00]     // set pin PB3 to output mode in GPIOB MODER register 
+    ldr     r0, =0x48000400     // load address of GPIOB peripheral block to r0
+    ldr     r1, [r0, #0x00]     // load content of GPIOB MODER register to r1
+    movs    r2, 0xC0            // load bit mask for pin PB3 mode bits to r2
+    bics    r1, r2              // clear all mode bits for pin PB3 (only)
+    movs    r2, 0x40            // load output mode bits for pin PB3 to r2
+    orrs    r1, r2              // set mode bits for pin PB3 (only) to GPIO output mode
+    str     r1, [r0, #0x00]     // store modified value to GPIOB MODER register
     
 blink:
-    movs    r1, 8               // load bit mask for pin PB3 to register r1
-    ldr     r2, [r0, 0x14]      // load (indirect) GPIOB ODR register value to r2
-    eors    r2, r1              // xor with bit mask to toggle PB3 (LED)
-    str     r2, [r0, 0x14]      // store (indirect) modified value to GPIOB ODR register
-    bl      delay               // call the delay function below 
+    ldr     r1, [r0, 0x14]      // load content of GPIOB ODR register to r1
+    movs    r2, #8              // load bit mask for pin PB3 output level to r2
+    eors    r1, r2              // xor with bit mask to toggle PB3 (LED) output level
+    str     r1, [r0, 0x14]      // store modified value to GPIOB ODR register
+    bl      delay               // call the delay function (see below) 
     b       blink               // branch to (goto) label blink (infinite loop)
 
 
@@ -46,6 +50,6 @@ blink:
 delay:                          // delay function, waste some time by counting down
     ldr     r2, =500000         // load a larger value to register r2
 loop:
-    subs    r2, #1              // subtract 1 from register r2 (count down)
-    bne     loop                // branch to lable loop if result of subs was not 0 (loop)
-    bx      lr                  // return to caller
+    subs    r2, #1              // subtract 1 from register r2
+    bne     loop                // branch to lable 'loop' if result of subs was not 0
+    bx      lr                  // return to caller (when r2 reaches 0)
